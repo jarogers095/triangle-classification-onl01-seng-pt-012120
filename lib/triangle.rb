bbacbda83404da2d9015aef906d0f01d3ec41e57
James Rogers <jarogers095@gmail.com>
2020-02-18 05:17:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
 class Triangle
-  # write code here
+  attr_reader(:sides)
+  
+  def initialize(side1, side2, side3)
+    @sides = []
+    @sides << side1
+    @sides << side2
+    @sides << side3
+  end
+  
+  def kind()
+    
+  end
+    
+  def e_tri()
+
+  end
+
+  def i_tri()
+    
+  end
+  
+  def s_tri()
+    
+  end
+  
+  def illegal_tri()
+    result = false
+    
+    if @sides.any?(0)
+      result = true
+    elsif @sides.any?
+      
+  end
+    
+  class TriangleError < StandardError
+    
+  end
 end
